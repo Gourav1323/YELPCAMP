dd9047d73a15a8f66887425f4836db4d88da97cd
Gourav1323 <156842888+Gourav1323@users.noreply.github.com>
2024-03-21 01:29:24 +0530

    Add files via upload

--- routes/campgorunds.js
+++ routes/campgorunds.js
@@ -3,84 +3,25 @@ const router = express.Router();
 const catchAsync = require('../utils/catchAsunc')
 const ExpressError = require('../utils/ExpressError');
 const Campground = require('../models/capmgroung');
-const {campgroundSchema}=require('../schemas');
-const {isloggedin} = require('./middleware');
+const campgrounds = require('../controllers/campgrounds');
 
-const validateCampground= (req,res,next)=>{
+const {isloggedin,validateCampground,isAuthor} = require('../middleware');
+router.route('/')
+.get(catchAsync(campgrounds.index))
+.post(isloggedin,validateCampground,catchAsync(campgrounds.createNew))
 
-    const {error} =campgroundSchema.validate(req.body)
-    if(error){
-        const msg =error.details.map(el=>el.message).join(',')
-    throw new ExpressError(msg,400)
-    }else{
-        next();
-    }
-    // console.log(error)
+router.get('/new',isloggedin,campgrounds.new)
 
-}
+router.route('/:id')
+.get(catchAsync(campgrounds.show))
+.put(isloggedin,validateCampground,isAuthor,catchAsync(campgrounds.editCamp))
+.delete(isloggedin,catchAsync(campgrounds.deleteCampground))
 
-router.get('/',catchAsync(async (req,res)=>{
-    const campgrounds = await Campground.find({});
-    res.render('campgrounds/index',{campgrounds});
-
-}))
-
-router.get('/new',isloggedin,(req,res)=>{
-
-    res.render('campgrounds/new')
-})
-
-
-router.get('/:id',catchAsync(async (req,res)=>{
-   const campground = await Campground.findById(req.params.id).populate('reviews');
-
-        if(!campground){
-            req.flash('error','an error found check the url not found')
-           return res.redirect('/campgrounds')
-        }
-    res.render('campgrounds/show',{campground});
-}))
-router.get('/:id/edit',catchAsync(async (req,res)=>{
-    const campground = await Campground.findById(req.params.id);
-    if(!campground){
-        req.flash('error','an error found check the url not found')
-       return res.redirect('/campgrounds')
-    }
-    res.render('campgrounds/edit',{campground});
-}))
-
+router.get('/:id/edit',isloggedin,isAuthor,catchAsync(campgrounds.renderEdit))
 
 // router.get('/makeCampground', async (req,res) => {
 // const camp = new Campground({ title : 'MY BACKYARD',description: 'sasta version'});
 // await camp.save();
 // res.send(camp);
-
 // })
-router.post('/',isloggedin,validateCampground,catchAsync(async (req,res,next)=>{
-// if(!req.body.campground)throw new ExpressError('Invalid camp DATA',400)
-const campground = new Campground(req.body.campground);
-await campground.save();
-req.flash('success','successfully created a new campground')
-res.redirect(`/campgrounds/${campground._id}`);
-
-})
-)
-router.put('/:id',isloggedin,validateCampground,catchAsync(async (req,res,next)=>{
-
-    const { id } = req.params;
-
-  const campground132 =  await Campground.findByIdAndUpdate(id, { ...req.body.campground},{new :true});
-  req.flash('success',`successfully edited the ${campground132.title} campground`)
-
-  res.redirect(`/campgrounds/${campground132._id}`);
-
-    // next(e)
-}));
-
-router.delete('/:id',isloggedin,catchAsync(async (req,res)=>{
-    const {id} = req.params;
-    await Campground.findByIdAndDelete(id);
-    req.flash('success',`successfully deleted the  campground`)
-    res.redirect('/campgrounds');
-}))
 module.exports = router;
--- routes/review.js
+++ routes/review.js
@@ -6,37 +6,12 @@ const Review =require('../models/review')
 const Campground = require('../models/capmgroung');
 const ExpressError = require('../utils/ExpressError');
 const {reviewSchema}=require('../schemas');
+const {validateReview,isloggedin} =require('../middleware');
+const { CreateReview, DeleteReview } = require('../controllers/review');
 
 
-const validateReview =(req,res,next)=>{
-    const {error} = reviewSchema.validate(req.body);
-    if(error){
-        const msg =error.details.map(el=>el.message).join(',')
-    throw new ExpressError(msg,400)
-    }else{
-        next();
-    }
-    }
 
-router.post('/',validateReview,catchAsync(async (req,res)=>{
-    const {id} = req.params
+router.post('/',isloggedin,validateReview,catchAsync(CreateReview))
 
-        const newReview = await Campground.findById(id)
-        const review = new Review(req.body.review);
-        newReview.reviews.push(review);
-        await review.save();
-        await newReview.save();
-        req.flash('success',`successfully posted the review `)
-        res.redirect(`/campgrounds/${newReview._id}`);
-
-    }))
-    router.delete('/:reviewId',catchAsync(async (req,res)=>{
-        const {id,reviewId} = req.params;
-        await Campground.findByIdAndUpdate(id,{$pull:{reviews:reviewId}})
-
-        await Review.findByIdAndDelete(reviewId);
-        req.flash('success',`successfully deleted the review `)
-        res.redirect(`/campgrounds/${id}`)
-
-    }))
+    router.delete('/:reviewId',isloggedin,catchAsync(DeleteReview))
     module.exports = router;
